a03df4460aa470ef1da36a65cedde9eb15de9a87
Udit <easyprograming1994@gmail.com>
2024-03-03 14:21:26 +0530

    project setup

---
+++ src/main.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx';
+import Root from "./routes/root";
+import About from "./routes/about";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
